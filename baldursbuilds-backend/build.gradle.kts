620b91fb6e8ef1d8fae4e80c119da1e67864a9eb
Leonardo Michelutti <leonardo.smichelutti@gmail.com>
2024-08-04 18:56:38 -0300

    feat: Add new dependencies

--- baldursbuilds-backend/build.gradle.kts
+++ baldursbuilds-backend/build.gradle.kts
@@ -13,14 +13,22 @@ val quarkusPlatformArtifactId: String by project
 val quarkusPlatformVersion: String by project
 
 dependencies {
+    implementation("io.quarkus:quarkus-rest-jackson")
+    implementation("io.quarkus:quarkus-hibernate-orm-panache")
     implementation("io.quarkus:quarkus-jdbc-postgresql")
     implementation("io.quarkus:quarkus-flyway")
     implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
     implementation("io.quarkus:quarkus-arc")
     implementation("io.quarkus:quarkus-rest")
     implementation("org.flywaydb:flyway-database-postgresql")
+    implementation("org.hibernate:hibernate-validator:8.0.1.Final")
+    compileOnly("org.projectlombok:lombok:1.18.34")
+    annotationProcessor("org.projectlombok:lombok:1.18.34")
     testImplementation("io.quarkus:quarkus-junit5")
     testImplementation("io.rest-assured:rest-assured")
+    testCompileOnly("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
+
 }
 
 group = "com.baldursbuilds"
